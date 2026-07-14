7b432376940d99394367ac517ee586a550214a26
surprise2024-cpu <surprise2024@gmail.com>
2026-07-14 22:13:43 +0200

    update

--- src/components/PopularThisWeek/PopularThisWeek.tsx
+++ src/components/PopularThisWeek/PopularThisWeek.tsx
@@ -9,7 +9,11 @@ import human4 from '../../assets/3D-image4.png'
 import { Text } from '../Text/Text'
 
 const items = [
-  {}
+  {id: 1, title: 'Costa', author: 'Yang', price: '1.75 ETH', likes: '39k', image: human},
+  {id: 1, title: 'VERSACE', author: 'Antoni Tudisco', price: '2.45 ETH', likes: '48k', image: human2},
+  {id: 1, title: 'Digital Decade', author: 'Anthony gargasz', price: '2.45 ETH', likes: '50k', image: human3, featured: true},
+  {id: 1, title: 'Winter Jamz', author: 'Antoni Tudisco', price: '2.3 ETH', likes: '47k', image: human4},
+  {id: 1, title: 'Pastal Wifey', author: 'blake kathryn', price: '2.23 ETH', likes: '44k', image: human}
 ]
 
 export const PopularThisWeek: React.FC<any> = () => {
@@ -22,10 +26,23 @@ export const PopularThisWeek: React.FC<any> = () => {
         </div>
 
         <div className={styles.images}>
-            <img src={human} className={styles.image} />
-            <img src={human2} className={styles.image} />
-            <img src={human3} className={styles.image} />
-            <img src={human4} className={styles.image} />
+          {items.map((item) => (
+            <div key={item.id} className={styles.card}>
+              <img src={item.image} className={styles.image} alt={item.title} />
+
+              <div className={styles.overlay}>
+                <div>
+                    <div className={styles.title}>{item.title}</div>
+                    <div className={styles.author}>{item.author}</div>
+                </div>
+                <div className={styles.meta}>
+                    <div className={styles.price}>{item.price}</div>
+                    <div className={styles.likes}>{item.likes}</div>
+                </div>
+              </div>
+
+            </div>
+          ))}
             
         </div>
 
