bb2825ae58f575cd14633f9397a942e1699a70ba
surprise2024-cpu <surprise2024@gmail.com>
2026-07-15 21:16:52 +0200

    update

--- src/components/ExploreArtWorks/ExploreArtWorks.tsx
+++ src/components/ExploreArtWorks/ExploreArtWorks.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 import { ContentContainer } from '../ContentContainer'
 import styles from './ExploreArtWorks.module.css'
 import { HeroCardOverlay } from '../HeroCardOverlay/HeroCardOverlay'
-import human from '../../assets/3D-image2.png'
+import human from '../../assets/3D-image1.png'
+import human2 from '../../assets/3D-image2.png'
+import human3 from '../../assets/3D-image3.png'
+import human4 from '../../assets/3D-image4.png'
 import { Text } from '../Text/Text'
 import { ExploreArtWorksCard } from './ExploreArtWorksCard'
 
 const artwork = [
-  {id: 1, name: 'Abstract', items: '30 items'},
-  {id: 2, name: '3D Art', items: '45 items'},
-  {id: 3, name: 'Modern Art', items: '59 items'},
-  {id: 4, name: 'Game', items: '25 items'},
-  {id: 5, name: 'Graffiti', items: '36 items'},
-  {id: 6, name: 'Watercolor', items: '40 items'},
+  {id: 1, name: 'Abstract', items: '30 items', images: [human, human2, human3, human4]},
+  {id: 2, name: '3D Art', items: '45 items', images: [human, human2, human3, human4]},
+  {id: 3, name: 'Modern Art', items: '59 items', images: [human, human2, human3, human4]},
+  {id: 4, name: 'Game', items: '25 items', images: [human, human2, human3, human4]},
+  {id: 5, name: 'Graffiti', items: '36 items', images: [human, human2, human3, human4]},
+  {id: 6, name: 'Watercolor', items: '40 items', images: [human, human2, human3, human4]},
 ]
 
 export const ExploreArtWorks = () => {
@@ -25,16 +28,12 @@ export const ExploreArtWorks = () => {
         </div>
 
         <div className={styles['card-cont']}>
-
-            
                 
             {
               artwork.map((art) => (
-                <ExploreArtWorksCard img1={human} img2={human} img3={human} img4={human} name={art.name} items={art.items} />
+                <ExploreArtWorksCard key={art.id} img1={human} img2={human2} img3={human3} img4={human4} name={art.name} items={art.items} />
               ))
             }
-
-            
 
         </div>
 
