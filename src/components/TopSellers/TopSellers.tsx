40e280b9ab3d9d3f50b6d442faa7a3dd9fbd44b3
surprise2024-cpu <surprise2024@gmail.com>
2026-07-15 13:04:58 +0200

    update

--- src/components/TopSellers/TopSellers.tsx
+++ src/components/TopSellers/TopSellers.tsx
@@ -7,6 +7,23 @@ import human from '../../assets/3D-image2.png'
 import { Text } from '../Text/Text'
 import { TopSellerBar } from './TopSellerBar'
 
+const sellers = [
+  {id: 1, name: 'Leighton Kramer', price: '276.7 Eth' },
+  {id: 2, name: 'Haylie Arcand', price: '345.6 Eth' },
+  {id: 3, name: 'Bowen Higgins', price: '323.7 Eth' },
+  {id: 4, name: 'Saige fuentes', price: '347.7 Eth' },
+  {id: 5, name: 'Sophie Mclain', price: '230.6 Eth' },
+  
+]
+
+const sellers2 = [
+  {id: 6, name: 'Jeremy Burch', price: '267.9 Eth' },
+  {id: 7, name: 'Amelie Griffith', price: '334.1 Eth' },
+  {id: 8, name: 'Iasbela Hart', price: '289.1 Eth' },
+  {id: 9, name: 'Diego Bentley', price: '290.7 Eth' },
+  {id: 10, name: 'Daisy Arnord', price: '265.4 Eth' }
+]
+
 export const TopSellers = () => {
   return (
 
@@ -16,8 +33,38 @@ export const TopSellers = () => {
         </div>
 
         <div className={styles['card-container']}>
-            <TopSellerBar image={human} name={'Leighton Kramer'} price={'276.7 Eth'} />
-            <TopSellerBar image={human} name={'Leighton Kramer'} price={'276.7 Eth'} />
+          <div className={styles['seller-track']}>
+
+            {
+              sellers.map((sel) => (
+                <TopSellerBar image={human} name={sel.name} price={sel.price} />
+              ))
+            }
+
+            {
+              sellers.map((sel) => (
+                <TopSellerBar image={human} name={sel.name} price={sel.price} />
+              ))
+            }
+            
+          </div>
+
+          <div className={styles['seller-track2']}>
+
+          {
+            sellers.map((sel) => (
+              <TopSellerBar image={human} name={sel.name} price={sel.price} />
+            ))
+          }
+
+          {
+            sellers.map((sel) => (
+              <TopSellerBar image={human} name={sel.name} price={sel.price} />
+            ))
+          }
+
+          </div>
+
         </div>
 
     </ContentContainer>
