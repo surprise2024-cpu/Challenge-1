b725ec077fe3b7a79dbe787868d12237350e575a
surprise2024-cpu <surprise2024@gmail.com>
2026-07-19 15:57:32 +0200

    update

--- src/components/Band/Band.tsx
+++ src/components/Band/Band.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 import styles from './Band.module.css'
 import { Bandbar } from './Bandbar'
 
+import binance from '../../assets/icons8-binance-64.png'
+import blockchain from '../../assets/icons8-blockchain-logo-50.png'
+import bitfinex from '../../assets/icons8-leaf-50.png'
+import exodus from '../../assets/icons8-x-vpn-50.png'
+
 const partners = [
     {id: 1, name: 'Paypal'},
     {id: 2, name: 'coinbase'},
-    {id: 3, name: 'BINANCE'},
+    {id: 3, name: 'BINANCE', img: binance},
     {id: 4, name: 'Revolut'},
-    {id: 5, name: 'EXODUS'},
-    {id: 6, name: 'BITFINEX'},
-    {id: 7, name: 'BlockChain'}
+    {id: 5, name: 'EXODUS', img: exodus},
+    {id: 6, name: 'BITFINEX', img2: bitfinex},
+    {id: 7, name: 'BlockChain', img: blockchain}
 ]
 
 export const Band = () => {
@@ -22,7 +27,7 @@ export const Band = () => {
                 [0, 1, 2, 3].map((setIndex) => (
                     partners.map((partner) => (
 
-                        <Bandbar key={`${setIndex}-${partner.id}`}  name={partner.name} />
+                        <Bandbar key={`${setIndex}-${partner.id}`}  name={partner.name} img={partner.img} img2={partner.img2}/>
 
                     ))
                     
