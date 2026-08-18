12f97f6cfc4c656c4d755ca751e5ee7e3a72918e
surprise2024-cpu <surprise2024@gmail.com>
2026-08-18 21:20:41 +0200

    Fix TopSellers import path to include file extension for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Band } from './components/Band/Band';
 import { PopularThisWeek } from './components/PopularThisWeek/PopularThisWeek';
 import { ExploreArtWorks } from './components/ExploreArtWorks/ExploreArtWorks';
 import { Community } from './components/Community/Community';
-import { TopSellers } from './components/TopSellers/TopSellers';
+import { TopSellers } from './components/TopSellers/TopSellers.tsx';
 
 
 function App() {
