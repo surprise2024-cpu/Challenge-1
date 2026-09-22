e38c5f3876d83e20c8dd30b04ddbe8aa88a1b63b
surprise2024-cpu <surprise2024@gmail.com>
2026-09-22 13:20:14 +0200

    Update CSS for Navbar and ContentContainer components to enhance layout consistency

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -8,7 +8,8 @@ type Props = {
 export const ContentContainer: React.FC<Props> = ({ children, className }) => {
   return (
     <div 
-      className={`content-container ${className} ?? '' `} 
+      className={`content-container ${className} ?? '' `}
+      style={{ maxWidth: 1200, margin: 'auto' }} 
     >
       {children}
 
