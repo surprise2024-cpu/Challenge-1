efc0871a5c085126a01b18dbe6e7b22d6c1223a8
surprise2024-cpu <surprise2024@gmail.com>
2026-09-23 10:04:37 +0200

    Add comment for future implementation in Register component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Overlay }  from '../Overlay/Overlay'
 
 
-
+// Reserved for future implementation
 export const Register: React.FC = () => {
   return (
     <Overlay>
